486b2e3615f58fe7a881d2695bcdf5b292ea9293
Georgi Zahariev <74987305+Georgi-Zahariev@users.noreply.github.com>
2025-06-24 17:00:09 +0300

    Custom logger with correlation ID

--- backend/handlers/user_handlers.go
+++ backend/handlers/user_handlers.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/Georgi-Zahariev/online-restaurant/backend/managers"
+	"github.com/Georgi-Zahariev/online-restaurant/backend/middlewares"
 	"github.com/Georgi-Zahariev/online-restaurant/backend/models"
 	"github.com/gorilla/mux"
 )
@@ -15,6 +17,8 @@ type GenericHandler struct {
 }
 
 func (h *GenericHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	logger := middlewares.GetLogger(r.Context())
+	logger.Info("GetAll called", "entity", h.Entity)
 	data, err := h.Manager.GetAll(h.Entity)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -24,7 +28,9 @@ func (h *GenericHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *GenericHandler) Get(w http.ResponseWriter, r *http.Request) {
+	logger := middlewares.GetLogger(r.Context())
 	id := mux.Vars(r)["id"]
+	logger.Info("Get called", "entity", h.Entity, "id", id)
 	data, err := h.Manager.Get(h.Entity, id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
@@ -34,89 +40,123 @@ func (h *GenericHandler) Get(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *GenericHandler) Create(w http.ResponseWriter, r *http.Request) {
+	logger := middlewares.GetLogger(r.Context())
+	logger.Info("Create called", "entity", h.Entity)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
 	var instance interface{}
 
 	switch h.Entity {
 	case "users":
-		user := models.User{}
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		var user models.User
+		if err := json.Unmarshal(body, &user); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = user
 	case "dishes":
-		dish := models.Dish{}
-		if err := json.NewDecoder(r.Body).Decode(&dish); err != nil {
+		var dish models.Dish
+		if err := json.Unmarshal(body, &dish); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = dish
 	case "orders":
-		order := models.Order{}
-		if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+		var order models.Order
+		if err := json.Unmarshal(body, &order); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = order
 	default:
+		logger.Error("Unknown entity", "entity", h.Entity)
 		http.Error(w, "Unknown entity", http.StatusBadRequest)
 		return
 	}
 
 	if err := h.Manager.Create(h.Entity, instance); err != nil {
+		logger.Error("Create failed", "error", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(instance)
+	response, _ := json.Marshal(instance)
+	w.Write(response)
+
+	logger.Info("Create successful", "entity", h.Entity)
 }
 
 func (h *GenericHandler) Update(w http.ResponseWriter, r *http.Request) {
+	logger := middlewares.GetLogger(r.Context())
 	id := mux.Vars(r)["id"]
+	logger.Info("Update called", "entity", h.Entity, "id", id)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		logger.Error("Failed to read request body", "error", err)
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
 	var instance interface{}
 
 	switch h.Entity {
 	case "users":
-		user := models.User{}
-		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		var user models.User
+		if err := json.Unmarshal(body, &user); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = user
 	case "dishes":
-		dish := models.Dish{}
-		if err := json.NewDecoder(r.Body).Decode(&dish); err != nil {
+		var dish models.Dish
+		if err := json.Unmarshal(body, &dish); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = dish
 	case "orders":
-		order := models.Order{}
-		if err := json.NewDecoder(r.Body).Decode(&order); err != nil {
+		var order models.Order
+		if err := json.Unmarshal(body, &order); err != nil {
+			logger.Error("Invalid JSON input", "error", err)
 			http.Error(w, "Invalid JSON input", http.StatusBadRequest)
 			return
 		}
 		instance = order
 	default:
+		logger.Error("Unknown entity", "entity", h.Entity)
 		http.Error(w, "Unknown entity", http.StatusBadRequest)
 		return
 	}
 
 	if err := h.Manager.Update(h.Entity, id, instance); err != nil {
+		logger.Error("Update failed", "error", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(instance)
+	response, _ := json.Marshal(instance)
+	w.Write(response)
+	logger.Info("Update successful", "entity", h.Entity, "id", id)
 }
 
 func (h *GenericHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	logger := middlewares.GetLogger(r.Context())
 	id := mux.Vars(r)["id"]
+	logger.Info("Delete called", "entity", h.Entity, "id", id)
 	if err := h.Manager.Delete(h.Entity, id); err != nil {
+		logger.Error("Delete failed", "error", err, "id", id)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
+	logger.Info("Delete successful", "entity", h.Entity, "id", id)
 }
--- backend/middlewares/middlewares.go
+++ backend/middlewares/middlewares.go
@@ -1,8 +1,12 @@
 package middlewares
 
 import (
+	"context"
 	"encoding/json"
+	"log/slog"
 	"net/http"
+
+	"github.com/gofrs/uuid/v5"
 )
 
 // Middleware to set "Content-Type" to "application/json"
@@ -25,4 +29,26 @@ func AuthorizationMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+type ctxKeyLogger struct{}
+
+var LoggerKey = ctxKeyLogger{}
+
+// Middleware to add a logger with correlation ID to the context
+func LoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqID := uuid.Must(uuid.NewV4()).String()
+		logger := slog.Default().With("correlation_id", reqID)
+		ctx := context.WithValue(r.Context(), LoggerKey, logger)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// Helper to get logger from context
+func GetLogger(ctx context.Context) *slog.Logger {
+	if logger, ok := ctx.Value(LoggerKey).(*slog.Logger); ok {
+		return logger
+	}
+	return slog.Default()
 }
